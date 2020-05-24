70193bdfbbb416b8ff50a0e2a3175f82aefa7540
olaszgbor <olaszgbor@gmail.com>
2020-05-24 15:19:50 +0200

    added executable jar

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,23 +1,9 @@
 package main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main {
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Valaszto.fxml"));
-        Parent root = loader.load();
-        stage.setTitle("Asdatbazis");
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Application.launch(HaziProjektApplication.class, args);
     }
 }
